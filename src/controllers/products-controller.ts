2eb451e867fdc9342832096fc6d77f1454124f84
Khellermann Machado <khellermann@gmail.com>
2024-12-11 14:50:27 -0300

    Validando dados do produto

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,5 +1,5 @@
 import {NextFunction, Request, Response} from 'express'
-import { AppError } from '@/utils/AppError';
+import {object, z} from 'zod';
 
 class ProductController{
     async index(request: Request, response: Response, next: NextFunction){
@@ -7,6 +7,24 @@ class ProductController{
             return response.json({message: "OK"});
         } catch(error){
             next(error);
+        }
+    }
+
+    async create(request: Request, response: Response, next: NextFunction){
+        try{
+
+            const bodySchema = z.object({
+                name: z.string({required_error: "nome é obrigátorio"}).trim().min(6),
+                price: z.number().gt(0, {message: "valor deve ser maior do que zero"} )
+            })
+
+            const {name, price} = bodySchema.parse(request.body);
+
+            return response.status(201).json({ name, price})
+        }
+        catch(error){
+            next(error);
+
         }
     }
 }
--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -5,5 +5,7 @@ const productsRoutes = Router();
 const productsController = new ProductController();
 
 productsRoutes.get("/", productsController.index);
+productsRoutes.post("/", productsController.create);
+
 
 export { productsRoutes };
