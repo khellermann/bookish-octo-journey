36bb67766f31ee11e76d48fa66dfa48cbf0e2470
Khellermann Machado <khellermann@gmail.com>
2024-12-11 13:58:45 -0300

    Instalando e Configurando Knex.js

---
+++ knexfile.ts
@@ -0,0 +1,16 @@
+
+export default {
+  client: "sqlite3",
+  connection: {
+    filename: "./src/database/database.db",
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extensions: ["ts"],
+    directory: "./src/database/migrations",
+  },
+  seeds: {
+    extensions: ["ts"],
+    directory: "./src/database/seeders",
+  },
+};
