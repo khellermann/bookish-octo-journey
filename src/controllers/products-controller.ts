dd6e733337a3215cfc2954f0512689b30d981180
Khellermann Machado <khellermann@gmail.com>
2024-12-11 17:04:53 -0300

    Listando produtos

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -5,7 +5,14 @@ import { z } from "zod";
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json({ message: "OK" });
+      const { name } = request.query;
+
+      const products = await knex<ProductRepository>("products")
+        .select()
+        .whereLike("name", `%${name ?? ""}%`)
+        .orderBy("name");
+
+      return response.json(products);
     } catch (error) {
       next(error);
     }
